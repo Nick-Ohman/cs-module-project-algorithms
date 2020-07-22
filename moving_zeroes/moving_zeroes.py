50b3cf0544a2a060af7d1eaba66ad0513191ea1a
Nick Ohman <nickohman@Nicks-MacBook-Pro.local>
2020-07-22 17:46:08 -0500

    movingzeros done

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,10 +2,20 @@
 Input: a List of integers
 Returns: a List of integers
 '''
+
+# we need to move the 0 ints to the end of list
+# could i pop off the 0's and re append them?
+
+
 def moving_zeroes(arr):
     # Your code here
 
-    pass
+    for i in arr:
+        if i == 0:
+            arr.remove(i)
+            arr.append(i)
+    return arr
+
 
 
 if __name__ == '__main__':
